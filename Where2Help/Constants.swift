97e7bab99d896bef70dfffcb14f2d9893484f581
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-05-19 17:50:40 +0200

    Extract API URL constant

---
+++ Where2Help/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Where2Help
+//
+//  Created by Ramon Huidobro on 19/05/16.
+//  Copyright © 2016 Where2Help. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+  static let Where2HelpAPIUrl = "http://staging-where2help.herokuapp.com/api/v1"
+}
