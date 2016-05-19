d62c08b0186b0e96ff80dd82d5e0a66f757592ed
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-05-19 15:56:34 +0200

    Add SignInViewController class

---
+++ Where2Help/Controllers/SignInViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.swift
+//  Where2Help
+//
+//  Created by Ramon Huidobro on 19/05/16.
+//  Copyright © 2016 Where2Help. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+}
