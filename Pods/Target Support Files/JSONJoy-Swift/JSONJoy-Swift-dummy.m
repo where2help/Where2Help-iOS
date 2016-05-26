6a95210827a9d1740cf5b6581cc7ab26beffc688
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-05-26 14:40:16 +0200

    Remove Pods folder

--- Pods/Target Support Files/JSONJoy-Swift/JSONJoy-Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JSONJoy_Swift : NSObject
-@end
-@implementation PodsDummy_JSONJoy_Swift
-@end
--- Pods/Target Support Files/Pods-Where2Help/Pods-Where2Help-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Where2Help : NSObject
-@end
-@implementation PodsDummy_Pods_Where2Help
-@end
