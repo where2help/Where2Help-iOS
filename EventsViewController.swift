bf822fb4eac660d2683b703ef63e87a287e69745
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-06-02 16:02:20 +0200

    Re-indent

--- EventsViewController.swift
+++ EventsViewController.swift
@@ -9,73 +9,72 @@
 import UIKit
 
 class EventsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, EventsHandler, UICollectionViewDelegateFlowLayout {
+  let kEventCellNibName = "EventCollectionViewCell"
+  let kEventCellID = "EventCell"
+  let kShiftDetailControllerID = "shiftDetailViewController"
 
-    let kEventCellNibName = "EventCollectionViewCell"
-    let kEventCellID = "EventCell"
-    let kShiftDetailControllerID = "shiftDetailViewController"
-    
-    let interactor = EventsInteractor()
-    
-    var detailViewController: ShiftDetailViewController!
-    
-    @IBOutlet weak var filterPickerControl: UISegmentedControl!
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupEventCell()
-        setupDetailController()
-//        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-//        let w = view.frame.width - 30
-//        flowLayout.itemSize =
-        interactor.setup(handler: self)
-        
-        
-    }
-    
-    private func setupEventCell() {
-        let nib = UINib(nibName: kEventCellNibName, bundle: nil)
-        collectionView.registerNib(nib, forCellWithReuseIdentifier: kEventCellID)
-    }
-    
-    private func setupDetailController() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        detailViewController = storyboard
-            .instantiateViewControllerWithIdentifier(kShiftDetailControllerID) as! ShiftDetailViewController
-    }
-    
-    // Mark - handlers
-    
-    func handleEventsUpdate() {
-        print("Events updated")
-        collectionView.reloadData()
-    }
-    
-    // MARK - TableViewDelegate/DataSource
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(kEventCellID, forIndexPath: indexPath) as! EventCollectionViewCell
-        let event = interactor.eventFor(indexPath)
-        cell.setEvent(event)
-        cell.navigationController = navigationController
-        cell.detailViewController = detailViewController
-        return cell
-    }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return interactor.eventCount()
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let event = interactor.eventFor(indexPath)
-        let multiplier = event.shifts.count
-        let h = CGFloat(80 + (multiplier * 50))
-        let w = view.frame.width - 30
-        return CGSize(width: w, height: h)
-    }
-    
-    
-    @IBAction func filterPickerTapped(sender: UISegmentedControl) {
-        interactor.filterList(index: sender.selectedSegmentIndex)
-    }
+  let interactor = EventsInteractor()
+
+  var detailViewController: ShiftDetailViewController!
+
+  @IBOutlet weak var filterPickerControl: UISegmentedControl!
+  @IBOutlet weak var collectionView: UICollectionView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupEventCell()
+    setupDetailController()
+    //        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+    //        let w = view.frame.width - 30
+    //        flowLayout.itemSize =
+    interactor.setup(handler: self)
+
+
+  }
+
+  private func setupEventCell() {
+    let nib = UINib(nibName: kEventCellNibName, bundle: nil)
+    collectionView.registerNib(nib, forCellWithReuseIdentifier: kEventCellID)
+  }
+
+  private func setupDetailController() {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    detailViewController = storyboard
+      .instantiateViewControllerWithIdentifier(kShiftDetailControllerID) as! ShiftDetailViewController
+  }
+
+  // Mark - handlers
+
+  func handleEventsUpdate() {
+    print("Events updated")
+    collectionView.reloadData()
+  }
+
+  // MARK - TableViewDelegate/DataSource
+
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(kEventCellID, forIndexPath: indexPath) as! EventCollectionViewCell
+    let event = interactor.eventFor(indexPath)
+    cell.setEvent(event)
+    cell.navigationController = navigationController
+    cell.detailViewController = detailViewController
+    return cell
+  }
+
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return interactor.eventCount()
+  }
+
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    let event = interactor.eventFor(indexPath)
+    let multiplier = event.shifts.count
+    let h = CGFloat(80 + (multiplier * 50))
+    let w = view.frame.width - 30
+    return CGSize(width: w, height: h)
+  }
+
+
+  @IBAction func filterPickerTapped(sender: UISegmentedControl) {
+    interactor.filterList(index: sender.selectedSegmentIndex)
+  }
 }
