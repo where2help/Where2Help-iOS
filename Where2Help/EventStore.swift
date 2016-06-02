297b25c3e961fd9353da15c48846133c29f43647
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-06-02 16:10:58 +0200

    Re-indent

--- Where2Help/EventStore.swift
+++ Where2Help/EventStore.swift
@@ -10,50 +10,51 @@ import Foundation
 import PromiseKit
 
 class EventStore {
-    func filter(filterName: ListFilter) -> Promise<[Event]> {
-        switch filterName {
-        case .Current:
-            return current()
-        case .Past:
-            return past()
-        case .All:
-            return all()
-        }
+  func filter(filterName: ListFilter) -> Promise<[Event]> {
+    switch filterName {
+    case .Current:
+      return current()
+    case .Past:
+      return past()
+    case .All:
+      return all()
     }
-    
-    private func current() -> Promise<[Event]> {
-        return Promise { resolve, reject in
-            let url = NSBundle.mainBundle().URLForResource("shifts-current", withExtension: "json")
-            let data = NSData(contentsOfURL: url!)
-            if let events = EventMapper.map(data!) {
-                resolve(events)
-                return
-            }
-            resolve([])
-        }
+  }
+
+  private func current() -> Promise<[Event]> {
+    return Promise { resolve, reject in
+
+      let url = NSBundle.mainBundle().URLForResource("shifts-current", withExtension: "json")
+      let data = NSData(contentsOfURL: url!)
+      if let events = EventMapper.map(data!) {
+        resolve(events)
+        return
+      }
+      resolve([])
     }
-    
-    private func all() -> Promise<[Event]> {
-        return Promise { resolve, reject in
-            let url = NSBundle.mainBundle().URLForResource("shifts-all", withExtension: "json")
-            let data = NSData(contentsOfURL: url!)
-            if let events = EventMapper.map(data!) {
-                resolve(events)
-                return
-            }
-            resolve([])
-        }
+  }
+
+  private func all() -> Promise<[Event]> {
+    return Promise { resolve, reject in
+      let url = NSBundle.mainBundle().URLForResource("shifts-all", withExtension: "json")
+      let data = NSData(contentsOfURL: url!)
+      if let events = EventMapper.map(data!) {
+        resolve(events)
+        return
+      }
+      resolve([])
     }
-    
-    private func past() -> Promise<[Event]> {
-        return Promise { resolve, reject in
-            let url = NSBundle.mainBundle().URLForResource("shifts-past", withExtension: "json")
-            let data = NSData(contentsOfURL: url!)
-            if let events = EventMapper.map(data!) {
-                resolve(events)
-                return
-            }
-            resolve([])
-        }
+  }
+
+  private func past() -> Promise<[Event]> {
+    return Promise { resolve, reject in
+      let url = NSBundle.mainBundle().URLForResource("shifts-past", withExtension: "json")
+      let data = NSData(contentsOfURL: url!)
+      if let events = EventMapper.map(data!) {
+        resolve(events)
+        return
+      }
+      resolve([])
     }
+  }
 }
