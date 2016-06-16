12dd009bbd794c1eb340136b0b8baae7cd8e0655
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-06-16 15:59:12 +0200

    Extract theme for colors

---
+++ Where2Help/Helpers/Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Theme.swift
+//  Where2Help
+//
+//  Created by Ramon Huidobro on 16/06/16.
+//  Copyright © 2016 Where2Help. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+  static func optInColor() -> UIColor {
+    return UIColor(red:0.23, green:0.69, blue:0.85, alpha:1.0)
+  }
+
+  static func optOutColor() -> UIColor {
+    return UIColor(red:0.85, green:0.27, blue:0.33, alpha:1.0)
+  }
+}
