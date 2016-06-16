0fe4921b64eee90fa5be6fbfaa4c92ed6a9d621d
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-06-16 15:42:58 +0200

    Re-indent

--- Where2Help/ShiftDetailViewController.swift
+++ Where2Help/ShiftDetailViewController.swift
@@ -10,72 +10,72 @@ import UIKit
 import MapKit
 
 class ShiftDetailViewController: UIViewController, ShiftHandler {
-    let interactor = ShiftDetailInteractor()
-    var event: Event!
-    var shift: Shift!
+  let interactor = ShiftDetailInteractor()
+  var event: Event!
+  var shift: Shift!
 
-    @IBOutlet weak var volunteerCountLabel: UILabel!
-    @IBOutlet weak var eventTitleLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var organizationNameLabel: UILabel!
-    @IBOutlet weak var descriptionTextView: UITextView!
-    
-    @IBOutlet weak var imInButton: UIButton!
-    
-    @IBOutlet weak var mapView: MKMapView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        interactor.setup(self, event: event, shift: shift)
-    }
-    
-    // MARK: - ShiftHandler
-    
-    func handleNewEvent(event: EventPresenter) {
-        updateEventFields(event)
-    }
-    
-    func handleNewShift(shift: ShiftPresenter) {
-        updateShiftFields(shift)
-    }
-    
-    func handleGeo(geo: EventGeo) {
-        print("Setting up map")
-        MapOperation.run(geo: geo, mapView: mapView)
-    }
-    
-    // MARK: - Actions
-    
-    @IBAction func imInPressed(sender: UIButton) {
-        interactor.toggleOptIn()
-    }
-    
-    // MARK: - UI Updates
-    
-    private func updateEventFields(event: EventPresenter) {
-        print("Updating event fields")
-        eventTitleLabel.text = event.title
-        descriptionTextView.text = event.description
-        locationLabel.text = event.location
-        organizationNameLabel.text = event.organization
-    }
-    
-    private func updateShiftFields(shift: ShiftPresenter) {
-        print("Updating shift fields")
-        timeLabel.text = shift.timespanLabelText()
-        dateLabel.text = shift.dateString()
-        volunteerCountLabel.text = "\(shift.volunteersOutOfLabelText()) Volunteers"
-    }
-    
-    ///////////////////////////////////
-    
-    deinit {
-        print("Deinit shift detail view controller")
-    }
+  @IBOutlet weak var volunteerCountLabel: UILabel!
+  @IBOutlet weak var eventTitleLabel: UILabel!
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var organizationNameLabel: UILabel!
+  @IBOutlet weak var descriptionTextView: UITextView!
+
+  @IBOutlet weak var imInButton: OptInAndOutButton!
+
+  @IBOutlet weak var mapView: MKMapView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    interactor.setup(self, event: event, shift: shift)
+  }
+
+  // MARK: - ShiftHandler
+
+  func handleNewEvent(event: EventPresenter) {
+    updateEventFields(event)
+  }
+
+  func handleNewShift(shift: ShiftPresenter) {
+    updateShiftFields(shift)
+  }
+
+  func handleGeo(geo: EventGeo) {
+    print("Setting up map")
+    MapOperation.run(geo: geo, mapView: mapView)
+  }
+
+  // MARK: - Actions
+
+  @IBAction func imInPressed(sender: UIButton) {
+    interactor.toggleOptIn()
+  }
+
+  // MARK: - UI Updates
+
+  private func updateEventFields(event: EventPresenter) {
+    print("Updating event fields")
+    eventTitleLabel.text = event.title
+    descriptionTextView.text = event.description
+    locationLabel.text = event.location
+    organizationNameLabel.text = event.organization
+  }
+
+  private func updateShiftFields(shift: ShiftPresenter) {
+    print("Updating shift fields")
+    timeLabel.text = shift.timespanLabelText()
+    dateLabel.text = shift.dateString()
+    volunteerCountLabel.text = "\(shift.volunteersOutOfLabelText()) Volunteers"
+  }
+
+  ///////////////////////////////////
+
+  deinit {
+    print("Deinit shift detail view controller")
+  }
 }
