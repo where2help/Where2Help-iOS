f0637e4bc4701b0c03ce426f46330a4d9b46e6c7
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2016-06-23 15:42:03 +0200

    Movie ProfileTableViewController into folder

--- Where2Help/Controllers/ProfileTableViewController.swift
+++ Where2Help/Controllers/ProfileTableViewController.swift
@@ -28,16 +28,16 @@ class ProfileTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
 
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
